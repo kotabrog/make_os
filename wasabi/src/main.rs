57f2e9d661e0993b83066b0f149aa6cad2453d3d
kota suzuki <suzuki.kota314@gmail.com>
2025-11-26 13:59:00 +0900

    ページテーブルの読み込み

--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -52,7 +52,14 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 
     let cr3 = wasabi::x86::read_cr3();
     println!("CR3: {cr3:#p}");
-    hexdump(unsafe { &*cr3 });
+    let t = Some(unsafe { &*cr3 });
+    println!("{t:?}");
+    let t = t.and_then(|t| t.next_level(0));
+    println!("{t:?}");
+    let t = t.and_then(|t| t.next_level(0));
+    println!("{t:?}");
+    let t = t.and_then(|t| t.next_level(0));
+    println!("{t:?}");
 
     loop {
         hlt()
