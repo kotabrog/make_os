b9d47804a4abb7eb89e8cee3bc6676313e38793b
kota suzuki <suzuki.kota314@gmail.com>
2026-03-25 08:44:28 +0900

    リファクタリングと細かな調整

--- wasabi/src/init.rs
+++ wasabi/src/init.rs
@@ -1,8 +1,13 @@
 extern crate alloc;
 
+use crate::acpi::AcpiRsdpStruct;
 use crate::allocator::ALLOCATOR;
+use crate::graphics::{draw_test_pattern, fill_rect, Bitmap};
+use crate::hpet::{set_global_hpet, Hpet};
+use crate::info;
 use crate::uefi::{
-    exit_from_efi_boot_services, EfiHandle, EfiMemoryType::*, EfiSystemTable, MemoryMapHolder,
+    exit_from_efi_boot_services, EfiHandle, EfiMemoryType, EfiMemoryType::*, EfiSystemTable,
+    MemoryMapHolder, VramBufferInfo,
 };
 use crate::x86::{write_cr3, PageAttr, PAGE_SIZE, PML4};
 use alloc::boxed::Box;
@@ -35,7 +40,41 @@ pub fn init_paging(memory_type: &MemoryMapHolder) {
     table
         .create_mapping(0, end_of_mem, 0, PageAttr::ReadWriteKernel)
         .expect("Failed to create initial page mapping");
+    // Unmap page 0 to detect null ptr dereference
+    table
+        .create_mapping(0, 4096, 0, PageAttr::NotPresent)
+        .expect("Failed to unmap page 0");
     unsafe {
         write_cr3(Box::into_raw(table));
     }
+}
+
+pub fn init_hpet(acpi: &AcpiRsdpStruct) {
+    let hpet = acpi.hpet().expect("Failed to get HPET from ACPI");
+    let hpet = hpet
+        .base_address()
+        .expect("Failed to get HPET base address");
+    info!("HPET is at {hpet:#p}");
+    let hpet = Hpet::new(hpet);
+    set_global_hpet(hpet);
+}
+
+pub fn init_allocator(memory_map: &MemoryMapHolder) {
+    let mut total_memory_pages = 0;
+    for e in memory_map.iter() {
+        if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
+            continue;
+        }
+        total_memory_pages += e.number_of_pages();
+        info!("{e:?}");
+    }
+    let total_memory_size_mib = total_memory_pages * 4096 / 1024 / 1024;
+    info!("Total memory: {total_memory_pages} pages = {total_memory_size_mib} MiB");
+}
+
+pub fn init_display(vram: &mut VramBufferInfo) {
+    let vw = vram.width();
+    let vh = vram.height();
+    fill_rect(vram, 0x000000, 0, 0, vw, vh).expect("Failed to fill rect");
+    draw_test_pattern(vram);
 }
--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -2,19 +2,13 @@
 #![no_main]
 #![feature(offset_of)]
 
-use core::fmt::Write;
 use core::time::Duration;
-use core::writeln;
 use wasabi::executor::{Executor, Task, TimeoutFuture};
-use wasabi::graphics::{draw_test_pattern, fill_rect, Bitmap};
-use wasabi::hpet::{global_timestamp, set_global_hpet, Hpet};
-use wasabi::init::{init_basic_runtime, init_paging};
-use wasabi::print::hexdump;
-use wasabi::uefi::{
-    init_vram, locate_loaded_image_protocol, EfiHandle, EfiMemoryType, EfiSystemTable,
-    VramTextWriter,
-};
-use wasabi::x86::{flush_tlb, init_exceptions, read_cr3, trigger_debug_interrupt, PageAttr};
+use wasabi::hpet::global_timestamp;
+use wasabi::init::{init_allocator, init_basic_runtime, init_display, init_hpet, init_paging};
+use wasabi::print::{hexdump, set_global_vram_writer};
+use wasabi::uefi::{init_vram, locate_loaded_image_protocol, EfiHandle, EfiSystemTable};
+use wasabi::x86::init_exceptions;
 use wasabi::{error, info, println, warn};
 
 #[no_mangle]
@@ -35,70 +29,23 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 
     let mut vram = init_vram(efi_system_table).expect("Failed to initialize VRAM");
 
-    let vw = vram.width();
-    let vh = vram.height();
-    fill_rect(&mut vram, 0x000000, 0, 0, vw, vh).expect("Failed to fill rect");
+    init_display(&mut vram);
 
-    draw_test_pattern(&mut vram);
-
-    let mut w = VramTextWriter::new(&mut vram);
+    set_global_vram_writer(vram);
 
     let acpi = efi_system_table.acpi_table().expect("ACPI table not found");
 
     let memory_map = init_basic_runtime(image_handle, efi_system_table);
 
-    let mut total_memory_pages = 0;
-    for e in memory_map.iter() {
-        if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
-            continue;
-        }
-        total_memory_pages += e.number_of_pages();
-        writeln!(w, "{e:?}").unwrap();
-    }
-    let total_memory_size_mib = total_memory_pages * 4096 / 1024 / 1024;
-    writeln!(
-        w,
-        "Total: {total_memory_pages} pages = {total_memory_size_mib} MiB"
-    )
-    .unwrap();
+    info!("Hello, Non-UEFI World!");
 
-    writeln!(w, "Hello, Non-UEFI World!").unwrap();
-
-    let cr3 = wasabi::x86::read_cr3();
-    println!("CR3: {cr3:#p}");
-    let t = Some(unsafe { &*cr3 });
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
+    init_allocator(&memory_map);
 
     let (_gdt, _idt) = init_exceptions();
-    info!("Exception initialized!");
-    trigger_debug_interrupt();
-    info!("Execution continued");
-
     init_paging(&memory_map);
-    info!("Now we are using our own page tables!");
 
-    // Unmap page 0 ti detect null ptr dereference
-    let page_table = read_cr3();
-    unsafe {
-        (*page_table)
-            .create_mapping(0, 4096, 0, PageAttr::NotPresent)
-            .expect("Failed to unmap page 0");
-    }
-    flush_tlb();
+    init_hpet(acpi);
 
-    let hpet = acpi.hpet().expect("Failed to get HPET from ACPI");
-    let hpet = hpet
-        .base_address()
-        .expect("Failed to get HPET base address");
-    info!("HPET is at {hpet:#p}");
-    let hpet = Hpet::new(hpet);
-    set_global_hpet(hpet);
     let t0 = global_timestamp();
 
     let task1 = Task::new(async move {
