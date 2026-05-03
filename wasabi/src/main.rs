4bb9faa57a5700e2670dca67f2684d0b563b76f4
kota suzuki <suzuki.kota314@gmail.com>
2026-05-04 07:20:14 +0900

    並列処理をglobalに移行

--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -3,7 +3,7 @@
 #![feature(offset_of)]
 
 use core::time::Duration;
-use wasabi::executor::{Executor, Task, TimeoutFuture};
+use wasabi::executor::{sleep, spawn_global, start_global_executor};
 use wasabi::hpet::global_timestamp;
 use wasabi::init::{
     init_allocator, init_basic_runtime, init_display, init_hpet, init_paging, init_pci,
@@ -52,22 +52,21 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 
     let t0 = global_timestamp();
 
-    let task1 = Task::new(async move {
+    let task1 = async move {
         for i in 100..=103 {
             info!("{i} hpet.main_counter = {:?}", global_timestamp() - t0);
-            TimeoutFuture::new(Duration::from_secs(1)).await;
+            sleep(Duration::from_secs(1)).await;
         }
         Ok(())
-    });
-    let task2 = Task::new(async move {
+    };
+    let task2 = async move {
         for i in 200..=203 {
             info!("{i} hpet.main_counter = {:?}", global_timestamp() - t0);
-            TimeoutFuture::new(Duration::from_secs(1)).await;
+            sleep(Duration::from_secs(1)).await;
         }
         Ok(())
-    });
-
-    let serial_task = Task::new(async {
+    };
+    let serial_task = async {
         let sp = SerialPort::default();
         if let Err(e) = sp.loopback_test() {
             error!("{e:?}");
@@ -79,15 +78,14 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
                 let c = core::char::from_u32(v as u32);
                 info!("serial input: {v:#04X} {c:?}");
             }
-            TimeoutFuture::new(Duration::from_millis(20)).await;
+            sleep(Duration::from_millis(20)).await;
         }
-    });
+    };
 
-    let mut executor = Executor::new();
-    executor.enqueue(task1);
-    executor.enqueue(task2);
-    executor.enqueue(serial_task);
-    Executor::run(executor);
+    spawn_global(task1);
+    spawn_global(task2);
+    spawn_global(serial_task);
+    start_global_executor();
 }
 
 #[cfg(all(not(test), not(feature = "std")))]
