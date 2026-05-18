ef2908fa3eb15af250836ea72a3ba8743ce39aa9
kota suzuki <suzuki.kota314@gmail.com>
2026-05-18 09:48:44 +0900

    xhciの情報取得

--- wasabi/src/pci.rs
+++ wasabi/src/pci.rs
@@ -1,6 +1,7 @@
 use crate::acpi::AcpiMcfgDescriptor;
 use crate::info;
 use crate::result::Result;
+use crate::xhci::PciXhciDriver;
 use core::fmt;
 use core::marker::PhantomData;
 use core::mem::size_of;
@@ -170,6 +171,9 @@ impl Pci {
         for bdf in BusDeviceFunction::iter() {
             if let Some(vd) = self.read_vendor_device_id(bdf) {
                 info!("{vd}");
+                if PciXhciDriver::supports(vd) {
+                    PciXhciDriver::attach(bdf);
+                }
             }
         }
     }
